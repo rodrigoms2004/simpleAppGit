5b071f98d65539df31232020f665a56013b30cf5
rodrigoms2004@gmail.com <rodrigoms2004@gmail.com>
2022-07-02 18:42:21 -0300

    added body-parser and cors support

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(cors())
+
+// Tell the bodyparser middleware to accept more data
+app.use(bodyParser.json({limit: '50mb'}))
 
 app.get('/', (req, res) => {
     res.send('hello world')
