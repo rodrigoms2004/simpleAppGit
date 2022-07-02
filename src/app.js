79418a26b976e7f74541cdd59fd21320ef620b05
rodrigoms2004@gmail.com <rodrigoms2004@gmail.com>
2022-07-02 18:44:50 -0300

    route does not exist middleware created

--- src/app.js
+++ src/app.js
@@ -12,5 +12,25 @@ app.get('/', (req, res) => {
     res.send('hello world')
 })
 
+// middleware to deal with 404 error
+app.use((req, res, next) => {
+    let err = {
+      message: 'route does not exist',
+      status: 404
+    }
+    //let err = new Error('route does not exist')
+    //err.status(404)
+    next(err)  // send error to next middleware
+})
+
+// receives error from last middleware
+app.use((err, req, res, next) => {
+    // if error 404, sends back message 'route does not exist'
+    // otherwise it sends Murphy's message
+    // log('api_server', 'error', `Error status ${err.status}, message: ${err.message}, url: ${req.url}.`)
+    // console.log(err.status)
+    res.status(err.status || 500).send(err.message || `Don't force it get a larger hammer.`)
+})
+
 app.listen(3000)
 
